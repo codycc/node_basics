13a69c46a28dfeebf59d960886df3beacfc34695
Cody Condon <codycondon50@gmail.com>
2016-06-17 12:57:21 -0400

    Exports vs module exports

---
+++ exports_vs_module_exports/app.js
@@ -0,0 +1,10 @@
+// var greet = require('./greet');
+// var greet2 = require('./greet2');
+// greet2.greet(); 
+
+
+// TESTING OUT UTIL
+// var util = require('util');
+// var name = 'cody';
+// var greeting = util.format('Hello, %s', name); 
+// util.log(greeting);
